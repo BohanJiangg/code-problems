0320150acf79ed9793831963f9007dfa59872f43
Bohan Jiang <bohandongqi@gmail.com>
2019-07-02 20:56:57 -0400

    Added coin change

--- coin_change_II.py
+++ coin_change_II.py
@@ -7,8 +7,8 @@ Leetcode: Coin Change 2
 
 You are given coins of different denominations and a total amount of money. Write a function to compute the number of combinations that make up that amount. You may assume that you have infinite number of each kind of coin.
 
-Time Complexity: O()
-Space Complexity: O(), 
+Time Complexity: O(n)
+Space Complexity: O(n), where n is size of amount + 1 
 
 @author: bohan
 
@@ -22,18 +22,16 @@ class Solution(object):
         :rtype: int
         """
         
-        wip = [amount]
-        result = 0 
+        combinations = [0]*(amount+1)
         
-        while wip:
-            temp = []
-            print(wip)
-            for currSum in wip:
-                for coin in coins:
-                    if currSum - coin == 0 :
-                        result += 1
-                    elif currSum - coin > 0:
-                        temp.append(currSum - coin)
-            wip = list(dict.fromkeys(temp) ) 
+        combinations[0] = 1
         
-        return result 
+        for coin in coins:
+            for idx in range(1, len(combinations)):
+                if idx >= coin:
+                    combinations[idx] += combinations[idx - coin]
+                
+        return combinations[amount]
+        
+        
+        
