a8ee226bd7d22dfd34a73a0a56b15a9381732df8
Bohan Jiang <bohandongqi@gmail.com>
2019-07-03 20:07:14 -0400

    solved PS

--- perfect_squares.py
+++ perfect_squares.py
@@ -6,8 +6,11 @@ Leetcode: Perfect Squares
 
 Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
 
-Time Complexity: O()
-Space Complexity: O(), 
+Time Complexity: O(n^2), 
+    The depth of the BFS is at most 4 by the four-square theorem (every natural number can be represented as the sum of four integer squares)
+    At every depth there will be at most sqrt(n) children, because there's at most sqrt(n) perfect squares that are smaller than n
+    (sqrt(n))^4 = n²
+Space Complexity: O(n^2), 
 
 @author: bohan
 
@@ -20,21 +23,38 @@ class Solution(object):
         :rtype: int
         """
         
-        largestPS = int(n**(1/2))
-        
-        if largestPS ** largestPS == n:
+        if n < 2:
             return 1
         
-        least = n
+        listPS = []
+        
+        i = 1
+        while i*i <= n:
+            listPS.append(i*i)
+            i+=1
+        
+        if listPS[-1]**2 == n:
+            return 1
+        
+        count = 0
+        q = [n]
+        
+        while q:
+            count += 1
+            temp = []
+            for curr in q:
+                
+                for ps in listPS:
+                    if curr - ps == 0:
+                        return count
+                    elif curr - ps > 0:
+                        temp.append(curr - ps)
+            q = temp
+        
+        return count
         
         
         
-        while largestPS > 1:
-            currSum = largestPS ** largestPS
-            remainder = n - currSum
-            largestPS = int(remainder**(1/2))
-            if largestPS == 1:
-            
             
         
         
