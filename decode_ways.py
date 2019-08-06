c646a00da16821be7921808a684db6fc3c04e963
bohanjiangg <bohandongqi@gmail.com>
2019-08-06 12:57:47 -0400

    unsolved decode ways

---
+++ decode_ways.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on August 6 2019
+Leetcode: Decode Ways
+
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given a non-empty string containing only digits, determine the total number of ways to decode it.
+
+Example 1:
+
+Input: "12"
+Output: 2
+Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+Example 2:
+
+Input: "226"
+Output: 3
+Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+
+Time Complexity O(n), 
+Space Complexity: O(1), 
+@author: bohan
+"""
+
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        if not s:
+            return 0
+        if len(s) == 1 and s != '0':
+            return 1
+
+        one_letters = 0
+        for ch in s:
+            if ch != '0':
+                one_letters+=1
+        
+        two_letters = 0
+        for i in range(1, len(s)):
+            if int()
+
+
+
+
+
+
+
+        
